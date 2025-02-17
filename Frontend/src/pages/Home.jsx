3ae514877f7090f852f17971df328b55b6ec50fd
Malay Maity <maitymalay27747@gmail.com>
2025-02-17 08:38:34 +0530

    commit

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,16 @@
+
+import axios from "axios";
+import { useState } from "react";
 export default function Home() {
+  const [userPrompt, setUserPrompt] = useState("");
+  const handleSubmit = () => {
+    console.log("clicked");
+    axios
+      .post("http://localhost:3000/generate-text", { prompt: userPrompt })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen  px-4">
       <h1 className="text-6xl font-bold text-center mb-4 max-w-5xl">
@@ -13,8 +25,12 @@ export default function Home() {
           type="text"
           placeholder="Enter the prompt"
           className="w-full p-4 pr-24 border-2 text-black rounded-lg focus:outline-none"
+          onChange={(e) => setUserPrompt(e.target.value)}
         />
-        <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+        <button
+          className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+          onClick={handleSubmit}
+        >
           Generate
         </button>
       </div>
